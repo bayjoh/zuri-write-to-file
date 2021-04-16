46ed3a198bbc63ae3eee2b25cfe847f804a1ef2f
bayjoh <pblead98@yahoo.com>
2021-04-16 01:32:10 +0100

    Updated the server.js file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # zuri-write-to-file
+
 API to consume data form a third party and write the result into a file
+
+#To Do
+
+Use node server.js to start the file
---
+++ server.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const http = require('http');
+  
+http.get("http://jsonplaceholder.typicode.com/posts", (res) => {
+
+    // initialize the container for our data
+    var data = "";
+
+    // this event fires many times, each time collecting another piece of the response
+    res.on("data", function (chunk) {
+        // append this chunk to our growing `data` var
+        data += chunk;
+    });
+
+    // this event fires *one* time, after all the `data` events/chunks have been gathered
+    res.on("end", () => {
+       
+        //Writes the result into posts.txt file
+        fs.writeFile("./result/posts.txt", data, (err) => {
+            if(err)
+                throw err;
+
+            console.log("Post File created successfully");
+        })
+    });
+    
+})
+//display error message if error exists
+.on('error', (e) => {
+  console.error(`Error: ${e.message}`);
+});;
+
+console.log("Server created successfully");
