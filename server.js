9c464c23a5e4a7739de4072936deea49b10f661d
bayjoh <pblead98@yahoo.com>
2021-04-18 07:45:30 +0100

    Check if rsult directory exists

--- server.js
+++ server.js
@@ -3,27 +3,35 @@ const http = require('http');
   
 http.get("http://jsonplaceholder.typicode.com/posts", (res) => {
 
-    // initialize the container for our data
-    var data = "";
+    //fs.access checks if directory exists
+    fs.access("./result", function(error) {
+        if (error) {
+            console.log("Directory does not exist.")
+        }
+        else {
+            // initialize the container for our data
+            var data = "";
 
-    // this event fires many times, each time collecting another piece of the response
-    res.on("data", function (chunk) {
-        // append this chunk to our growing `data` var
-        data += chunk;
-    });
+            // this event fires many times, each time collecting another piece of the response
+            res.on("data", function (chunk) {
+                // append this chunk to our growing `data` var
+                data += chunk;
+            });
+        
+            // this event fires *one* time, after all the `data` events/chunks have been gathered
+            res.on("end", () => {
+                
+                //Writes the result into posts.txt file
+                fs.writeFile("./result/posts.txt", data, (err) => {
+                    if(err)
+                        throw err;
+        
+                    console.log("Post File created successfully");
+                })
+            });
+        }
 
-    // this event fires *one* time, after all the `data` events/chunks have been gathered
-    res.on("end", () => {
-       
-        //Writes the result into posts.txt file
-        fs.writeFile("./result/posts.txt", data, (err) => {
-            if(err)
-                throw err;
-
-            console.log("Post File created successfully");
-        })
-    });
-    
+    });  
 })
 //display error message if error exists
 .on('error', (e) => {
